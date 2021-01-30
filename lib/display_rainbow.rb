41f2fadef131a22762ead524e08b0341db83f05e
Aashiyana Patel <aashiyanapatel@rocketmail.com>
2021-01-30 23:32:02 +0000

    Automatically backed up by Learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,6 +1,1 @@
-def display_rainbow(color = "red", "orange", "yellow", "green", "blue", "indigo", "violet")
-  print "#{color[0]}, #{color[1]}, #{color[2]}, #{color[3]}, #{color[4]}, #{color[5]}, #{color[6]}, #{color[8]}"
-  
-end
-
-colors = 
+def display_rainbow
