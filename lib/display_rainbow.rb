6b66895f0454b4c7e211d0829fcbb252901a21b4
Aashiyana Patel <aashiyanapatel@rocketmail.com>
2021-01-30 23:43:17 +0000

    Automatically backed up by Learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,3 +1,3 @@
 def display_rainbow(color)
-  puts "#{color[0]}, #{colo[1]}, #{color[2]}, #{color[3]}, #{color[4]}, #{color[5]}, #{color[6
+  puts "#{color[0]}, #{colo[1]}, #{color[2]}, #{color[3]}, #{color[4]}, #{color[5]}, #{color[6]}"
 end
