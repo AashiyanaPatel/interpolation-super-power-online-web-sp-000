ece53b3b0abb6408a440ea780bc5d0cb8aa9704b
Aashiyana Patel <aashiyanapatel@rocketmail.com>
2021-01-31 00:01:16 +0000

    Automatically backed up by Learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,5 +1,7 @@
 def display_rainbow(colors)
-  puts "colors = ["R: red", "O: orange", "Y: yellow", "G: green", "B: blue", "I: indigo", "V: violet"]"
+   
+  
+  colors = ["R: red", "O: orange", "Y: yellow", "G: green", "B: blue", "I: indigo", "V: violet"]"
 end
 
 display_rainbow(colors)
